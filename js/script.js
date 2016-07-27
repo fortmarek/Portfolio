45677310c8d6d47bb45c4134c1fc6733e2d7c56f
fortmarek <marekfort@me.com>
2016-07-27 15:08:24 +0200

    Added Spaces

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/<**
+/*
  * Created by marekfort on 13/04/16.
  */
 
@@ -7,12 +7,7 @@ $(document).ready(function(){
     // Setting summary to be centered to the iphone mockup
     $('.section-swipe .summary').css('margin-top', ($('.section-swipe img').height() / 2 - $('.section-swipe .summary').height() / 2 ));
     $('.section-supl .summary').css('margin-top', ($('.section-supl img').height() / 2 - $('.section-supl .summary').height() / 2 ));
-<<<<<<< HEAD
-
-=======
->>>>>>> simple-changes
 });
-
 
 lastScroll = 0;
 shouldAnimate = true;
@@ -56,7 +51,6 @@ $(window).on('scroll', function(){
         $('.slide').addClass('animated fadeInUp');
         shouldSlide = false;
     }
-
 });
 
 $('.portfolio-link').click(function() {
