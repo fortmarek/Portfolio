d719edc0e27dd751b9a8ee27dbcc3e9d4dcef7fb
fortmarek <marekfort@me.com>
2016-11-10 21:37:06 +0100

    check heading, animate color

--- js/script.js
+++ js/script.js
@@ -4,21 +4,29 @@
 
 
 isMobile = false;
-lastScroll = 0;
+lastScroll = 1000;
 lastColor = "#3C9BFF";
 pixelsForScroll = 0;
 translateValue = 0;
 shouldMove = true;
-summaryIndex = 0;
+enterIndex = 0;
+exitIndex = 0;
 colors = ["#3C9BFF", "#FF3D97"];
+summaryDict = {"supl-heading":"#3C9BFF", "swipe-heading": "#FF3D97"};
 
 waypoint = $(window).height() * 2.2;
 
 
-inView('.summary').on('enter', function() {
-    $('.content').css('background-color', (colors[summaryIndex]));
-    summaryIndex++;
-});
+inView('.summary h4')
+    .on('enter', function(heading) {
+
+        headingColor = summaryDict[heading.id];
+
+        if (headingColor != lastColor) {
+            $('.content').css('background-color', (headingColor));
+            lastColor = headingColor;
+        }
+    });
 
 inView('.section-me').once('enter', function() {
     $('.slide').addClass('animated fadeIn');
