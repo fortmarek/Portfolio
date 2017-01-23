c43c7e5760a3c3b8c8e5a901a69766208c010a66
fortmarek <marekfort@me.com>
2017-01-24 00:14:27 +0100

    deselect button

--- js/post.js
+++ js/post.js
@@ -8,7 +8,19 @@ selected_now = 0;
 $('.thumbnails li').click(function(e) {
     index = $(e.target.parentNode).index();
     thumb_list = $('.thumbnails');
+
+    deselected_button = get_button(selected_now);
+    deselected_button.removeClass('selected-thumb');
+
+    button = get_button(index);
+    button.addClass('selected-thumb');
+
+    selected_now = index;
+});
+
+
+function get_button(index) {
     thumb = thumb_list.children().eq(index);
     button = thumb.children('button');
-    button.addClass('selected-thumb');
-});
+    return button;
+}
