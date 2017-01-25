191ea3c20f4d8a6dbed0cd0e86933e9c2dcb338b
fortmarek <marekfort@me.com>
2017-01-26 00:16:57 +0100

    change background images

--- js/post.js
+++ js/post.js
@@ -7,6 +7,7 @@ selected_now = 0;
 
 $('.thumbnails li').click(function(e) {
     index = $(e.target.parentNode).index();
+
     thumb_list = $('.thumbnails');
 
     deselected_button = get_button(selected_now);
@@ -14,6 +15,17 @@ $('.thumbnails li').click(function(e) {
 
     button = get_button(index);
     button.addClass('selected-thumb');
+
+    slider_list = $('.slider');
+
+    deselected_img = slider_list.children().eq(selected_now);
+    deselected_img.css('opacity', ('0'));
+
+    img = slider_list.children().eq(index);
+    img.css('opacity', ('1'));
+
+
+
 
     selected_now = index;
 });
