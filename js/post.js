8367e52949e75c80d012f4d1adde44eec3c47cb1
fortmarek <marekfort@me.com>
2017-01-30 15:46:34 +0100

    slider_box change image

--- js/post.js
+++ js/post.js
@@ -5,8 +5,8 @@
 
 (function($) {
 
-    $.Slider = function() {
-        this.selected_now = 0;
+    $.Slider = function(el) {
+        this.selected_now = 2;
         this.thumb_list = $(this).find('.thumbnails');
         this.slider_list = $(this).find('.slider');
     };
@@ -16,41 +16,51 @@
 $('.thumbnails li').click(function(e) {
     index = $(e.target.parentNode).index();
 
-    change_image();
+    /*change_image();*/
 
     selected_now = index;
 });
 
 $('.right-arrow').click(function() {
-    slider_box = this.parent;
+    slider_box = $(this.parent);
     if (!(slider_box instanceof $.Slider)) {
-        this.parent = new $.Slider(slider_box);
+        console.log("FAIL");
+        this.parent = new $.Slider(this.parent);
+        slider_box = new $.Slider(slider_box);
     }
 
-    this.parent.selected_now += 1;
-    console.log(this.parent.selected_now);
+    console.log(slider_box.selected_now);
+
+    change_image(slider_box);
+
+    slider_box.selected_now += 1;
 
 });
 
 
 function change_image(slider_box) {
 
-    deselected_button = get_button(slider_box.selected_now);
+    /*deselected_button = get_button(slider_box.selected_now);*/
+    deselected_thumb = slider_box.thumb_list.find('li').eq(slider_box.selected_now);
+    deselected_button = deselected_thumb.children('button');
     deselected_button.removeClass('selected-thumb');
-
-    button = get_button(slider_box, index);
+    /*button = get_button(slider_box, index);*/
+    console.log(slider_box.thumb_list);
+    thumb = slider_box.thumb_list.find('li').eq(slider_box.selected_now);
+    button = thumb.children('button');
     button.addClass('selected-thumb');
 
     deselected_img = slider_box.slider_list.eq(slider_box.selected_now);
     deselected_img.css('opacity', ('0'));
 
-    img = slider_box.slider_list.eq(index);
+    img = slider_box.slider_list.eq(slider_box.selected_now);
     img.css('opacity', ('1'));
 }
 
 
+/*
 function get_button(slider_box, index) {
     thumb = slider_box.thumb_list.eq(index);
     button = thumb.children('button');
     return button;
-}
+}*/
