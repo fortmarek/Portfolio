3a8f06028135cbf1d244994fd124acf62eb0a9e3
fortmarek <marekfort@me.com>
2017-01-30 16:13:00 +0100

    selected_now count fixed

--- js/post.js
+++ js/post.js
@@ -6,7 +6,7 @@
 (function($) {
 
     $.Slider = function(el) {
-        this.selected_now = 2;
+        this.selected_now = 0;
         this.thumb_list = $(this).find('.thumbnails');
         this.slider_list = $(this).find('.slider');
     };
@@ -22,38 +22,33 @@ $('.thumbnails li').click(function(e) {
 });
 
 $('.right-arrow').click(function() {
-    slider_box = $(this.parent);
-    if (!(slider_box instanceof $.Slider)) {
-        console.log("FAIL");
+    if (!(this.parent instanceof $.Slider)) {
         this.parent = new $.Slider(this.parent);
-        slider_box = new $.Slider(slider_box);
+        console.log(this.parent.selected_now);
     }
 
-    console.log(slider_box.selected_now);
+    change_image(this.parent);
 
-    change_image(slider_box);
-
-    slider_box.selected_now += 1;
-
+    this.parent.selected_now += 1;
 });
 
 
 function change_image(slider_box) {
-
+    console.log(slider_box.selected_now);
     /*deselected_button = get_button(slider_box.selected_now);*/
     deselected_thumb = slider_box.thumb_list.find('li').eq(slider_box.selected_now);
     deselected_button = deselected_thumb.children('button');
     deselected_button.removeClass('selected-thumb');
     /*button = get_button(slider_box, index);*/
     console.log(slider_box.thumb_list);
-    thumb = slider_box.thumb_list.find('li').eq(slider_box.selected_now);
+    thumb = slider_box.thumb_list.find('li').eq(slider_box.selected_now + 1);
     button = thumb.children('button');
     button.addClass('selected-thumb');
 
     deselected_img = slider_box.slider_list.eq(slider_box.selected_now);
     deselected_img.css('opacity', ('0'));
 
-    img = slider_box.slider_list.eq(slider_box.selected_now);
+    img = slider_box.slider_list.eq(slider_box.selected_now + 1);
     img.css('opacity', ('1'));
 }
 
